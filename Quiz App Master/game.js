6a9db31aefe67ee233c6472bb6840664dd4d7dc2
Roovelo <rnjs8657@naver.com>
2023-02-28 15:53:35 +0900

    quzi api to local json

--- Quiz App Master/game.js
+++ Quiz App Master/game.js
@@ -20,26 +20,7 @@ fetch(
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion;
-        });
-
+        question = loadedQuestions;
         startGame();
     })
     .catch((err) => {
