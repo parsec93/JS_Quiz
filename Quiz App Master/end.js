fd7eb5ef528cba0bb63fd0079919817bd9e2dd4e
parsec93 <parsec93@naver.com>
2023-03-01 20:08:06 +0900

    img_down_update form일 경우 넘어가 버려서 img 저장이 안됐었음

--- Quiz App Master/end.js
+++ Quiz App Master/end.js
@@ -27,15 +27,12 @@ saveHighScore = (e) => {
     localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('https://js-quiz-dusky.vercel.app');
 };
-//const saveBtn = document.getElementById("saveScoreBtn");
-$('#saveScoreBtn').click((e) => {
-    html2canvas(document.getElementById("capture_area")).then(canvas => {
-        document.body.appendChild(canvas);
-    });
-   /* html2canvas(document.getElementById("capture_area")).then(function(canvas) {
-        const el = document.createElement("a")
+
+$('#saveScoreBtn').click(function(e){
+    html2canvas(document.getElementById("capture_area")).then(function(canvas) {
+        var el = document.createElement("a")
         el.href = canvas.toDataURL("image/jpeg")
-        el.download = '토잘알 퀴즈.jpg' //다운로드 할 파일명 설정
+        el.download = 'tohyeon_quiz.jpg' //다운로드 할 파일명 설정
         el.click()
-    })*/
+    });
 });
